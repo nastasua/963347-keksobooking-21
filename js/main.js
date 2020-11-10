e291d3b5fccda8773f23e3d2eab361f38a42ee25
nastasua <nastasyua.r@yandex.ru>
2020-11-10 22:27:58 +0300

    исправила ошибки, вывела аватар для метки

--- js/main.js
+++ js/main.js
@@ -6,54 +6,55 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function getRandomArray(array) {
+function getRandomLengthArrayFrom(array) {
   let randomLength = getRandomInt(1, array.length);
   let arrayValues = [];
-  for (var j = 0; j < array.length; j++) {
+  for (var j = 0; j < randomLength; j++) {
     arrayValues[j] = array[j];
   }
-  arrayValues.length = randomLength;
   return arrayValues;
 }
 
-function createArrayObjects() {
+function createObjects() {
+
+  let numArrayAvatars = [1, 2, 3, 4, 5, 6, 7, 8];
+  function createRandomAvatars() {
+    let numAvatar = numArrayAvatars.splice(0, 1);
+    return numAvatar;
+  }
 
   function createObject() {
-    // avatar без повторений пробовала найти указанным ниже способом, но ничего не получилось
-    // let numArrayAvatar = [1, 2, 3, 4, 5, 6, 7, 8];
-    // let numAvatar = getRandomInt(1, numArrayAvatar.length);
-    // numArrayAvatar.splice(numAvatar, 1);
-    let numAvatar = getRandomInt(1, 8);
-    let avatar = 'img/avatars/user' + '0' + numAvatar + '.' + 'png';
+
+    let avatar = 'img/avatars/user' + '0' + createRandomAvatars() + '.' + 'png';
 
     let title = 'Lorem Ipsum';
 
     let cost = getRandomInt(1000, 20000);
 
-    let arrayTypes = ['palace', 'flat', 'house', 'bungalow'];
-    let randomType = getRandomInt(0, arrayTypes.length);
+    const ARRAY_TYPES = ['palace', 'flat', 'house', 'bungalow'];
+    let randomType = getRandomInt(0, ARRAY_TYPES.length);
 
-    let type = arrayTypes[randomType];
+    let type = ARRAY_TYPES[randomType];
 
     let quantityRooms = getRandomInt(0, 400);
 
     let quantityGuests = getRandomInt(0, quantityRooms);
 
-    let arrayTimes = ['12:00', '13:00', '14:00'];
+    const ARRAY_TIMES = ['12:00', '13:00', '14:00'];
 
-    let randomCheckin = getRandomInt(0, arrayTimes.length);
-    let checkin = arrayTimes[randomCheckin];
+    let randomCheckin = getRandomInt(0, ARRAY_TIMES.length);
+    let checkin = ARRAY_TIMES[randomCheckin];
 
-    let randomCheckout = getRandomInt(0, arrayTimes.length);
-    let checkout = arrayTimes[randomCheckout];
+    let randomCheckout = getRandomInt(0, ARRAY_TIMES.length);
+    let checkout = ARRAY_TIMES[randomCheckout];
 
-    let arrayFeatures = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
-    let arrayFeaturesValues = getRandomArray(arrayFeatures);
+    const ARRAY_FEATURES = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
+    let arrayFeaturesValues = getRandomLengthArrayFrom(ARRAY_FEATURES);
 
     let description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
-    let arrayPhotos = ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"];
-    let arrayPhotosValues = getRandomArray(arrayPhotos);
+    const ARRAY_PHOTOS = ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"];
+    let arrayPhotosValues = getRandomLengthArrayFrom(ARRAY_PHOTOS);
 
     let locationX = getRandomInt(0, document.querySelector('.map').offsetWidth);
 
@@ -92,30 +93,30 @@ function createArrayObjects() {
   return objects;
 }
 
-let myArrayObjects = createArrayObjects();
+let myObjects = createObjects();
 
-function delClass(block, delBlockClass) {
-  block.classList.remove(delBlockClass);
+function deleteClass(block, deleteBlockClass) {
+  block.classList.remove(deleteBlockClass);
 }
 
-delClass(document.querySelector('.map'), 'map--faded');
+deleteClass(document.querySelector('.map'), 'map--faded');
 
-function createMapPin(myArray) {
+function createMapPin(entities) {
+  let fragment = document.createDocumentFragment();
   let containerMapPins = document.querySelector('.map__pins');
   let templateMapPin = document.querySelector('#pin').content.querySelector('.map__pin');
   let imgMapPin = templateMapPin.querySelector('img');
 
-  for (var k = 0; k < 8; k++) {
+  entities.forEach(function(entity) {
     let newMapPin = templateMapPin.cloneNode(true);
-    newMapPin.style.left = myArray[k].location.x + newMapPin.offsetWidth / 2 + 'px';
-    newMapPin.style.top = myArray[k].location.y + newMapPin.offsetHeight + 'px';
-    imgMapPin.src = myArray[k].author.avatar;
-    imgMapPin.alt = myArray[k].offer.title;
+    newMapPin.style.left = entity.location.x + newMapPin.offsetWidth / 2 + 'px';
+    newMapPin.style.top = entity.location.y + newMapPin.offsetHeight + 'px';
+    imgMapPin.src = entity.author.avatar;
+    imgMapPin.alt = entity.offer.title;
 
-    containerMapPins.appendChild(newMapPin);
-  }
+    fragment.appendChild(newMapPin);
+  });
+  containerMapPins.appendChild(fragment);
 }
 
-createMapPin(myArrayObjects);
-
-
+createMapPin(myObjects);
